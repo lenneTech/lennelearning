9c1350597165527adaad4141f4700ad62024cff3
Pascal Klesse <pascal.klesse@lenne.tech>
2020-06-08 00:25:26 +0200

    Update Packages

--- rename.sh
+++ rename.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 set -e
 
-if [ -z "$1" ]
-  then
-    echo "No old name supplied"
-    exit 1
-fi
-
 if [ -z "$2" ]
   then
     echo "No new name supplied"
     exit 1
 fi
 
-OLD_NAME=$1
 NEW_NAME=$1
 
 rm -rf node_modules
 
-sed -i "" "s/"$OLD_NAME"/"$NEW_NAME"/g" *
+sed -i "" "s/angular-website-template/"$NEW_NAME"/g" *
+
+sed -i "" "s/AngularWebsiteTemplate/"$NEW_NAME"/g" *
