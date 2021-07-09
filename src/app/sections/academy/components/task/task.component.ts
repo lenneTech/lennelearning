2f38100cc21fbddd2426db42cb366bdc727dc286
Pascal Klesse <pascal.klesse@lenne.tech>
2021-07-09 13:54:02 +0200

    Tooltips for tasks

--- src/app/sections/academy/components/task/task.component.ts
+++ src/app/sections/academy/components/task/task.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DialogService } from '../../../../modules/core/services/dialog.service';
 
 @Component({
   selector: 'task',
@@ -6,12 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task.component.scss'],
 })
 export class TaskComponent implements OnInit {
+  @Input() hint?: string;
+  @Input() videoReference?: string;
+  @Input() solutionLink?: string;
+
   completed = false;
-  constructor() {}
+  constructor(private dialogService: DialogService) {}
 
   ngOnInit(): void {}
 
   onComplete(): void {
     this.completed = !this.completed;
+  }
+
+  openHelp(): void {
+    this.dialogService.openHelperDialog('Hilfe', this.hint, this.videoReference, this.solutionLink);
   }
 }
