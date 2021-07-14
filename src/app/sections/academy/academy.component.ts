70d8f3ac052bbc1940bd0f8b8d2ebf071896ea95
FSteinhansesLT <82445251+FSteinhansesLT@users.noreply.github.com>
2021-07-14 15:59:15 +0200

    Sidebar/responsive (#42)
    * responsive sidebar with checked error
    
    * detect changes to fix error
    
    * return parameters
    
    * removed unnecessary modules

--- src/app/sections/academy/academy.component.ts
+++ src/app/sections/academy/academy.component.ts
@@ -1,26 +1,30 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterContentChecked, ChangeDetectorRef, Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { EntryPointService } from '../../modules/core/services/entry-point.service';
 import { Router } from '@angular/router';
 import { NbMenuItem } from '@nebular/theme';
 import { SectionService } from '../../modules/core/services/section.service';
 import { EntryPoint } from '../../modules/core/interfaces/entry-point.interface';
 import { Subscription } from 'rxjs';
+import { NbSidebarService } from '@nebular/theme';
 
 @Component({
   selector: 'app-academy',
   templateUrl: './academy.component.html',
   styleUrls: ['./academy.component.scss'],
 })
-export class AcademyComponent implements OnInit, OnDestroy {
+export class AcademyComponent implements OnInit, OnDestroy, AfterContentChecked {
   sections: string[] = [];
   items: NbMenuItem[] = [];
   currentEntryPoint: EntryPoint;
   subscriptions = new Subscription();
+  sidebarCollapsed = false;
 
   constructor(
     private entryPointService: EntryPointService,
     private router: Router,
-    private sectionService: SectionService
+    private sectionService: SectionService,
+    private sidebarService: NbSidebarService,
+    private ref: ChangeDetectorRef
   ) {
     // Get current entry point
     this.subscriptions.add(
@@ -34,29 +38,55 @@ export class AcademyComponent implements OnInit, OnDestroy {
       this.sectionService.currentSectionMenuItemsObservable.subscribe((items) => {
         // Refresh items and reset old children
         this.setSections();
-
         if (!items) {
           return;
         }
-
         const menuItem: NbMenuItem = this.items.find((item) => item.title === this.sectionService.currentSection);
-        if (menuItem) {
+        if (menuItem && window.screen.width >= 768) {
           menuItem.children = items;
         }
+        this.initSidebarCollapse();
       })
     );
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    if (event.target.innerWidth > 768) {
+      this.openSidebar();
+    } else {
+      this.closeSidebar();
+    }
+  }
+
   ngOnInit(): void {
     this.setSections();
-
     if (!window.location.href.includes('/academy/')) {
       this.router.navigate([`/academy/${this.sections[0]}`]);
     }
   }
 
+  ngAfterContentChecked(): void {
+    this.ref.detectChanges();
+  }
+
   ngOnDestroy(): void {
     this.subscriptions.unsubscribe();
+  }
+
+  toggleSidebar(): void {
+    this.sidebarService.toggle(false, 'left');
+    this.sidebarCollapsed = !this.sidebarCollapsed;
+  }
+
+  closeSidebar(): void {
+    this.sidebarService.collapse();
+    this.sidebarCollapsed = true;
+  }
+
+  openSidebar(): void {
+    this.sidebarService.expand();
+    this.sidebarCollapsed = false;
   }
 
   setSections(): void {
@@ -75,5 +105,13 @@ export class AcademyComponent implements OnInit, OnDestroy {
   resetEntryPoint(): void {
     this.entryPointService.reset();
     this.router.navigate(['/']);
+  }
+
+  initSidebarCollapse(): void {
+    // closed on mobile
+    this.sidebarCollapsed = false;
+    if (window.screen.width < 768) {
+      this.closeSidebar();
+    }
   }
 }
