aa2fee10d0c6d533174b38c88c6b61590259213b
florian.steinhanses <florian.steinhanses@lenne.tech>
2021-07-15 12:50:54 +0200

    information on company site

--- src/app/sections/landing-pages/company-page/company-page.component.ts
+++ src/app/sections/landing-pages/company-page/company-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TextImageInterface } from '../../../modules/core/interfaces/text-image.interface';
 
 @Component({
   selector: 'app-company-page',
@@ -6,10 +7,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./company-page.component.scss'],
 })
 export class CompanyPageComponent implements OnInit {
-  title = 'Du suchst fähige Entwickler für dein Unternehmen?';
+  title = 'Ihr sucht fähige Entwickler für euer Unternehmen?';
   subtitle =
-    'Wir betreuen und evaluieren deine potenziellen Entwickler im Web und App Bereich.\n In unserer Akademie lernen sie wie man mit aktuellen Technologien agile Web- und Appanwendungen entwickelt.';
+    'Wir betreuen und evaluieren eure potenziellen Entwickler im Web und App Bereich.\n In unserer Akademie lernen sie wie man mit aktuellen Technologien agile Web- und Appanwendungen entwickelt.';
   imageUrl = '../../assets/images/entry-point-images/company.svg';
+  steps: TextImageInterface[] = [
+    {
+      title: 'Vorbereitung',
+      text: 'Im Rahmen der Akademie bereiten wir eure Kandidaten optimal auf ihr Praktikum vor, sodass sie euch schnellstmöglich bei euren Projekten unterstützen können.',
+      imageUrl: '../../assets/images/explanation-images/academy.svg',
+    },
+    {
+      title: 'Evaluation',
+      text: 'Durch die persönliche Betreuung und die wöchentlichen Meetings können wir den Lernstand der Akademie Teilnehmer gut einschätzen und evaluieren, inwiefern sich ein Kandidat für euer Unternehmen eignet.',
+      imageUrl: '../../assets/images/explanation-images/strategy.svg',
+    },
+    {
+      title: 'Übernahme',
+      text: 'Auch nach der Übernahme in euer Unternehmen können die Absolventen Teil der Community bleiben und unsere Akademie nutzen. Wir bleiben gerne in Kontakt und helfen euch bei euren Projekten.',
+      imageUrl: '../../assets/images/explanation-images/community.svg',
+    },
+  ];
+  features: TextImageInterface[] = [
+    {
+      title: 'Einfach',
+      text: 'Wir übernehmen die Vorbereitung auf das Praktikum für euch. So könnt ihr Zeit sparen und euch aufs Wesentliche konzentrieren.',
+      iconClass: 'bi-x-diamond-fill',
+    },
+    {
+      title: 'Individuell',
+      text: 'Wir erstellen auch individuell Kurse für die Technologien mit denen ihr arbeitet. So werden die Lernpläne der Kandidaten für eure Ziele zugeschnitten.',
+      iconClass: 'bi-person-lines-fill',
+    },
+    {
+      title: 'Flexibel',
+      text: 'Die Akademie ist monatlich kündbar. So könnt ihr auch schon vorzeitig Kandidaten ins Praktikum überführen, wenn sie vom Lernstand bereit sind.',
+      iconClass: 'bi-calendar2-week-fill',
+    },
+    {
+      title: 'Ausbildung',
+      text: 'Wir bilden unabhängig von der Akademie auch im Bereich der Anwendungsentwicklung aus. Wenn euer Unternehmen Fachinformatiker benötigt, können wir die Ausbildung übernehmen.',
+      iconClass: 'bi-briefcase-fill',
+    },
+    {
+      title: 'Kooperation',
+      text: 'Bleibt auch in Zukunft mit uns in Kontakt und arbeitet mit uns an spannenden Projekten. Wir unterstützen euch gerne!',
+      iconClass: 'bi-people-fill',
+    },
+  ];
 
   constructor() {}
 
