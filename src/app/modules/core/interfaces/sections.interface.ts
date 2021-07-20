08e5fd7e557465dc344b4b11965cb66ea80b42bf
DKoenig9 <daniel.koenig@lenne.tech>
2021-07-20 12:42:13 +0200

    added requests

--- src/app/modules/core/interfaces/sections.interface.ts
+++ src/app/modules/core/interfaces/sections.interface.ts
@@ -1,14 +1,5 @@
 import { Tasks } from './tasks.interface';
 
 export interface Sections {
-  job?: Tasks[];
-  html?: Tasks[];
-  css?: Tasks[];
-  bootstrap?: Tasks[];
-  javascript?: Tasks[];
-  typescript?: Tasks[];
-  angular?: Tasks[];
-  gitBasics?: Tasks[];
-  gitExpert?: Tasks[];
-  nestJS?: Tasks[];
+  [key: string]: Tasks[];
 }
