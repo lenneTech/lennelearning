72cd09038f78abc9a0d457044640e1527372a744
florian.steinhanses <florian.steinhanses@lenne.tech>
2021-07-21 12:54:52 +0200

    fix

--- src/app/sections/academy/academy.module.ts
+++ src/app/sections/academy/academy.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { AcademyComponent } from './academy.component';
 import { AcademyRoutingModule } from './academy-routing.module';
 import {
-  NbAccordionModule,
   NbButtonModule,
   NbCardModule,
   NbLayoutModule,
