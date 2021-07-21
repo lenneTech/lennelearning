6ff6a411d7f9ab19ac15d914e83f0e58753017e2
DKoenig9 <daniel.koenig@lenne.tech>
2021-07-21 11:06:40 +0200

    removed dots

--- src/app/modules/core/services/section.service.ts
+++ src/app/modules/core/services/section.service.ts
@@ -62,38 +62,15 @@ export class SectionService {
         taskTitle = node.querySelector('.task-title').textContent;
       }
 
-      const color = this.getTaskType(node);
-
       // Generates the sub-items for the sections
       result.push({
         title: taskTitle,
         link: `/lernpfad/${this.currentSection}`,
         fragment: node.id,
-        badge: {
-          dotMode: true,
-          status: color,
-        },
       });
     });
 
     this.currentSectionMenuItems = result;
-  }
-
-  /**
-   * Checks which tasktype is used and generates a specific color for it
-   */
-  getTaskType(node): string {
-    if (node.querySelectorAll('task-text')[0]) {
-      return 'success';
-    } else if (node.querySelectorAll('task-video')[0]) {
-      return 'info';
-    } else if (node.querySelectorAll('task-questions')[0]) {
-      return 'primary';
-    } else if (node.querySelectorAll('task-code')[0]) {
-      return 'warning';
-    } else {
-      return 'danger';
-    }
   }
 
   setCurrentSection(section: string): boolean {
