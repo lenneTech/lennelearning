c48e318aafbec4101a97d2635f5c02bb33af552d
florian.steinhanses <florian.steinhanses@lenne.tech>
2021-08-11 12:19:08 +0200

    changes

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,7 +1,18 @@
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
+describe('Page Testing', () => {
+  it('Visits the home page', () => {
     cy.visit('/');
-    cy.contains('Welcome');
-    cy.contains('sandbox app is running!');
+    cy.contains('Willkommen');
+  });
+  it('Visits the job interested page', () => {
+    cy.visit('/berufsinteressierte');
+    cy.contains('Du interessierst dich für App- und Webentwicklung?');
+  });
+  it('Visits the intern page', () => {
+    cy.visit('/praktikanten');
+    cy.contains('Du suchst noch ein Praktikum?');
+  });
+  it('Visits the company page', () => {
+    cy.visit('/unternehmen');
+    cy.contains('Du suchst fähige Entwickler für dein Unternehmen?');
   });
 });
