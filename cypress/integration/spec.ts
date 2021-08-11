2245b7ba2c9a9fb093d3b76782a18d4eb99f20b2
FSteinhansesLT <82445251+FSteinhansesLT@users.noreply.github.com>
2021-08-12 01:08:32 +0200

    cypress installed (#89)
    * cypress installed
    
    * changes
    
    * Tests

---
+++ cypress/integration/spec.ts
@@ -0,0 +1,43 @@
+describe('Page Testing', () => {
+  it('Visits the Home Page', () => {
+    cy.visit('/');
+    cy.contains('Willkommen');
+  });
+  it('Visits the job interested page', () => {
+    cy.get('.learn-more').first().click();
+    cy.contains('Du interessierst dich für App- und Webentwicklung?');
+  });
+  it('Visits the intern page', () => {
+    cy.visit('/');
+    cy.get('.learn-more').eq(1).click();
+    cy.contains('Du suchst noch ein Praktikum?');
+  });
+  it('Visits the company page', () => {
+    cy.visit('/');
+    cy.get('.learn-more').last().click();
+    cy.contains('Du suchst fähige Entwickler für dein Unternehmen?');
+  });
+  it('Navigate to Academy Pages from Home Page', () => {
+    cy.visit('/');
+    cy.get('.link').last().click();
+  });
+  it('Check all Tasks', () => {
+    cy.get('.bi-check-circle').click({ multiple: true, force: true }).should('have.class', 'bi-check');
+    cy.get('.bi-trophy-fill').click();
+  });
+  it('Go through Milestone', () => {
+    cy.get('[data-test-id="milestone-next"]').click();
+    cy.get('[data-test-id="milestone-next"]').click();
+    cy.get('[data-test-id="milestone-next"]').click();
+    cy.get('[data-test-id="milestone-next-section"]').click();
+  });
+  it('Check Questionlist and Dialog', () => {
+    cy.get('.questionlist').first().click({ force: true }).should('have.class', 'question-done');
+    cy.get('.bi-question-circle').first().click({ force: true });
+    cy.get('[data-test-id="dialog-ok"]').click();
+  });
+  it('Check out', () => {
+    cy.get('.bi-door-open').first().click({ force: true });
+    cy.get('[data-test-id="dialog-confirm"]').click();
+  });
+});
