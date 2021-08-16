8e6b2f984818c0255dadb9825197d233692a83ba
DKoenig9 <daniel.koenig@lenne.tech>
2021-08-16 12:13:37 +0200

    disabled cookies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatomoTracker } from 'ngx-matomo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private matomoTracker: MatomoTracker) {}
+
+  ngOnInit() {
+    this.matomoTracker.disableCookies();
+  }
+}
