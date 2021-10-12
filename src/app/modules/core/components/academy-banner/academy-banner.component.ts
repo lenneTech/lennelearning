4897c37e4308e6f55ba8583d3efa01d3798375c2
Daniel König <daniel.koenig@lenne.tech>
2021-10-12 11:07:19 +0200

    added component

---
+++ src/app/modules/core/components/academy-banner/academy-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-academy-banner',
+  templateUrl: './academy-banner.component.html',
+  styleUrls: ['./academy-banner.component.scss'],
+})
+export class AcademyBannerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
