d80029e0040444fe3436609073728fbbb2142fc0
Robin Zeppenfeld <robinzeppenfeld@Air-von-Robin.fritz.box>
2022-02-15 15:20:29 +0100

    changes to the wording on the intern page

--- src/app/modules/core/components/text-image-list/text-image-list.component.ts
+++ src/app/modules/core/components/text-image-list/text-image-list.component.ts
@@ -11,30 +11,19 @@ export class TextImageListComponent {
   @Input() listElements: TextImageInterface[] = [
     {
       title: 'Akademie',
-      text:
-        'Durch Tutorials in Form von Texten, Videokursen und Spielen bekommst du einen Einstieg in die Grundlagen der App- & Webentwicklung. \n' +
-        '\n' +
-        'Unser Anliegen ist es, mit der Akademie jedem den idealen Einstieg in den Bereich der Softwareentwicklung zu bieten. Ganz ohne Vorkenntnisse. Um diesem Ziel gerecht zu werden, entwickeln wir die Akademie ständig weiter und freuen uns auf dein Feedback.\n' +
-        '\n' +
-        'Es wäre toll, wenn du uns bei der Weiterentwicklung unserer Akademie unterstützen würdest. Alles was du dafür brauchst, lernst du in der Akademie und kannst dein Wissen direkt anwenden, denn die Akademie ist selbst eine kleine Webanwendung.\n' +
-        'Das Repository der Akademie findest du <a class="link" href="https://github.com/lenneTech/academy" target="_blank">hier</a>.\n' +
-        '\n' +
-        'Wir freuen uns auf Deine Ideen, Optimierungsvorschläge und deinen Quellcode. Lass dich dabei von den vorhandenen Quellen inspirieren, aber bitte kopiere nichts. Auch wenn viele Dinge im Internet frei zugänglich sind, unterliegen sie dennoch dem Urheberrecht und dürfen nicht einfach auf anderen Seiten übernommen werden. Dies gilt für Bilder, Texte, Videos und andere urheberrechtlich geschützte Inhalte. Es gibt einige Seiten, die Inhalte zur freien Verwendung zur Verfügung stellen (z. B. Unsplash). Bitte prüfe, bevor du Inhalte einreichst die Lizenzbedingungen. Wenn du dir unsicher bist, spreche gerne deinen Tutor an.',
+      text: 'Durch Tutorials in Form von Texten, Videokursen und Spielen bekommst Du einen ersten Einstieg in die Grundlagen der App- und Webentwicklung. \n\nUnser Anliegen ist es, mit der Akademie allen Interessierten einen idealen Einstiegspunkt in den Bereich der Softwareentwicklung bieten zu können. Ob mit oder ohne Vorkenntnisse. \n\nUm dieses Ziel zu erreichen, entwickeln wir die Akademie ständig weiter und freuen uns auf Dein Feedback.',
       imageUrl: '../../assets/images/explanation-images/academy.svg',
       imageAlt: 'Illustration eines Mannes der auf Büchern steht.',
     },
     {
       title: 'Eigenes Projekt',
-      text:
-        'Nachdem du dir die Grundlagen angeeignet hast, erstellst du mit dem erlangten Wissen dein erstes vollständiges Projekt. ' +
-        'Dabei wirst du sowohl einen Client als auch einen Server bauen und diese miteinander verbinden. ' +
-        'Wichtige Bedingung: Es wird kein „Schubladenprojekt“. Daher suche dir ein Projekt aus, das für eine reale Zielgruppe einen echten Mehrwert bietet.',
+      text: 'Nachdem Du Dir die Grundlagen angeeignet hast, erstellst Du basierend auf Deinem neuen Wissen Dein erstes eigenes und vollständiges Projekt. Dabei wirst Du sowohl einen Client als auch einen Server bauen und diese miteinander verbinden. Wichtige Bedingung: Es wird kein „Schubladenprojekt“. Daher such Dir ein Projekt aus, das für eine reale Zielgruppe einen echten Mehrwert bietet.',
       imageUrl: '../../assets/images/explanation-images/projekt.svg',
       imageAlt: 'Illustration eines Mannes der malt.',
     },
     {
-      title: 'Kooperation',
-      text: 'Sobald du die ersten beiden Schritte erfolgreich gegangen bist, integrieren wir dich in ein aktives Team, sodass du auch Erfahrung in der Entwicklung größerer Projekte zusammen mit anderen sammeln kannst. Hier lernst du, wie man gemeinsam an einem Quellcode arbeitet, was Level 2 und Level 3 Tests sind, wie sich Verbesserungsvorschläge konstruktiv einbringen lassen und vieles mehr.',
+      title: 'Teamarbeit',
+      text: 'Sofern die Dauer des Praktikums es zulässt und Du die ersten beiden Schritte erfolgreich gegangen bist, integrieren wir Dich in ein aktives Projektteam. So sammelst Du Erfahrungen im Rahmen größerer Entwicklungsprojekte mit mehreren Beteiligten. Hier lernst Du z. B. wie gemeinsam an einem Quellcode gearbeitet wird, was Level 2 und Level 3 Tests sind, wie sich Verbesserungsvorschläge konstruktiv einbringen lassen und vieles mehr.',
       imageUrl: '../../assets/images/explanation-images/kooperation.svg',
       imageAlt: 'Illustration eines Mannes und einer Frau die Puzzleteile halten.',
     },
