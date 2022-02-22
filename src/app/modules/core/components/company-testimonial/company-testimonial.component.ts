8ff8d72b18cc7e1bf3cd0ebe40a4f9cc022d6f7d
Robin Zeppenfeld <robinzeppenfeld@Air-von-Robin.fritz.box>
2022-02-22 15:54:58 +0100

    feat(LTAK-8)additional-review

--- src/app/modules/core/components/company-testimonial/company-testimonial.component.ts
+++ src/app/modules/core/components/company-testimonial/company-testimonial.component.ts
@@ -13,6 +13,12 @@ export class CompanyTestimonialComponent {
     reviewerTitle: 'Christopher Klein & Jens Helbig \n Gründer und Geschäftsführer',
     reviewerSubtitle: 'Buuk GmbH',
   };
+  @Input() review_two: Review = {
+    imageUrl: '',
+    text: 'Was uns an lenne.Tech und der Akademie besonders beeindruckt hat, ist die Effektivität in der Umsetzung. Die natürliche Auslese beim Durchlaufen der Akademie sorgt für den höchstmöglichen Eignungsgrad der Bewerber*innen. Wir haben uns so nicht nur etliche Stunden Aufwand bei der Evaluierung von Bewerbern gespart, sondern auch jede Menge Nerven beim Onboarding.',
+    reviewerTitle: 'Christopher Klein & Jens Helbig \n Gründer und Geschäftsführer',
+    reviewerSubtitle: 'Buuk GmbH',
+  };
 
   constructor() {}
 }
