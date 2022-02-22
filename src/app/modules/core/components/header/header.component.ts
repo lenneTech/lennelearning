7ba7d16b9e19995a1c15b8108aa0cffb023fbe56
Daniel König <daniel.koenig@lenne.tech>
2022-02-22 11:08:41 +0100

    added navbar for main pages and academy

--- src/app/modules/core/components/header/header.component.ts
+++ src/app/modules/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  constructor() {}
+  openDropdown = false;
+  activatedRoute = false;
+  constructor(private router: Router) {
+    console.log(this.router.url);
+    if (
+      this.router.url === '/berufsinteressierte' ||
+      this.router.url === '/praktikanten' ||
+      this.router.url === '/unternehmen'
+    ) {
+      this.activatedRoute = true;
+    }
+  }
+
+  switch() {
+    this.openDropdown = !this.openDropdown;
+  }
 }
