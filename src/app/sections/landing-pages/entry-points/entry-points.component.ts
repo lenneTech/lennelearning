41397c2639e136e8de2b15e12e4442015f948ea2
Robin Zeppenfeld <robinzeppenfeld@Air-von-Robin.fritz.box>
2022-03-01 13:13:09 +0100

    feat(LTAK-5): added selection on entrypoint page

--- src/app/sections/landing-pages/entry-points/entry-points.component.ts
+++ src/app/sections/landing-pages/entry-points/entry-points.component.ts
@@ -4,6 +4,13 @@ import { Subscription } from 'rxjs';
 import { EntryPoint } from 'src/app/modules/core/interfaces/entry-point.interface';
 import { EntryPointService } from 'src/app/modules/core/services/entry-point.service';
 
+enum EntryPointMode {
+  ALL = 'ALL',
+  JOB_INTERESTED = 'JOB_INTERESTED',
+  INTERN = 'INTERN',
+  COMPANY = 'COMPANY',
+}
+
 @Component({
   selector: 'app-entry-points',
   templateUrl: './entry-points.component.html',
@@ -13,11 +20,14 @@ export class EntryPointsComponent implements OnInit {
   title = 'Die Lernpfade';
   subtitle =
     'Bist Du schon ziemlich erfahren oder stehst Du noch ganz am Anfang deiner Karriere als Entwickler:in \n Bei lenne.Learning findest Du das passende Angebot. Wähle einfach den Lernpfad aus, der am besten zu Dir passt und starte durch!';
-  imageUrl = '../../assets/images/explanation-images/projekt.svg';
+  imageUrl = '../../assets/images/banner-images/SETTINGS.svg';
   entryPoints: EntryPoint[] = [];
   recommendation: string;
   redirectionLink: string;
   routeSub: Subscription;
+  mode: EntryPointMode = EntryPointMode.ALL;
+  EntryPointMode = EntryPointMode;
+  filteredArray: EntryPoint[] = [];
 
   constructor(
     private entryPointService: EntryPointService,
@@ -29,6 +39,7 @@ export class EntryPointsComponent implements OnInit {
     this.getAllEntryPoints();
     this.getRecommendation();
     this.getRedirectionLink();
+    this.onButtonClicked(EntryPointMode.ALL);
   }
 
   getAllEntryPoints(): void {
@@ -50,5 +61,14 @@ export class EntryPointsComponent implements OnInit {
     this.activatedRoute.queryParams.subscribe((value) => {
       this.redirectionLink = value.redirectTo;
     });
+  }
+  onButtonClicked(mode: EntryPointMode) {
+    this.mode = mode;
+    console.log(this.entryPoints);
+    this.filteredArray = this.entryPoints.filter((el) => {
+      console.log(el.tag.includes(mode));
+      if (el.tag.includes(mode)) return el;
+    });
+    console.log(this.filteredArray);
   }
 }
