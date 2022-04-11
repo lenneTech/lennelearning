9c9351118763d9911f0992986b41afd2da1050c8
Daniel König <daniel.koenig@lenne.tech>
2022-04-11 13:31:56 +0200

    fix(LTAK-18):fixed styling bug of links in exercise-lists

--- src/app/sections/academy/directives/link.directive.ts
+++ src/app/sections/academy/directives/link.directive.ts
@@ -1,11 +1,22 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[taskLink]',
 })
 export class LinkDirective {
-  constructor(el: ElementRef) {
+  constructor(private el: ElementRef) {
     el.nativeElement.classList.add('link');
     el.nativeElement.target = '_blank';
+  }
+
+  @HostListener('click') onClick(): void {
+    const parent = this.el.nativeElement.parentNode;
+    const classList = parent.classList.value;
+
+    if (classList.includes('exercise-done')) {
+      parent.classList.remove('exercise-done');
+    } else {
+      parent.classList.add('exercise-done');
+    }
   }
 }
