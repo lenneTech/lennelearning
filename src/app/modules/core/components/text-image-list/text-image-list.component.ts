5a93fb6080c44efd87a79fd9b9ea6115a1040509
DKoenig9 <85439028+DKoenig9@users.noreply.github.com>
2022-04-26 13:58:57 +0200

    Feat/ltak 61 workshop page (#300)
    * feat(LTAK-61):created workshops-page with visual design

--- src/app/modules/core/components/text-image-list/text-image-list.component.ts
+++ src/app/modules/core/components/text-image-list/text-image-list.component.ts
@@ -8,26 +8,7 @@ import { TextImageInterface } from '../../interfaces/text-image.interface';
   encapsulation: ViewEncapsulation.None,
 })
 export class TextImageListComponent {
-  @Input() listElements: TextImageInterface[] = [
-    {
-      title: 'Akademie',
-      text: 'Durch Tutorials in Form von Texten, Videokursen und Spielen bekommst Du einen ersten Einstieg in die Grundlagen der App- und Webentwicklung. \n\nUnser Anliegen ist es, mit der Akademie allen Interessierten einen idealen Einstiegspunkt in den Bereich der Softwareentwicklung bieten zu können. Ob mit oder ohne Vorkenntnisse. \n\nUm dieses Ziel zu erreichen, entwickeln wir die Akademie ständig weiter und freuen uns auf Dein Feedback.',
-      imageUrl: '../../assets/images/explanation-images/academy.svg',
-      imageAlt: 'Illustration eines Mannes der auf Büchern steht.',
-    },
-    {
-      title: 'Eigenes Projekt',
-      text: 'Nachdem Du Dir die Grundlagen angeeignet hast, erstellst Du basierend auf Deinem neuen Wissen Dein erstes eigenes und vollständiges Projekt. Dabei wirst Du sowohl einen Client als auch einen Server bauen und diese miteinander verbinden. Wichtige Bedingung: Es wird kein „Schubladenprojekt“. Daher such Dir ein Projekt aus, das für eine reale Zielgruppe einen echten Mehrwert bietet.',
-      imageUrl: '../../assets/images/explanation-images/projekt.svg',
-      imageAlt: 'Illustration eines Mannes der malt.',
-    },
-    {
-      title: 'Teamarbeit',
-      text: 'Sofern die Dauer des Praktikums es zulässt und Du die ersten beiden Schritte erfolgreich gegangen bist, integrieren wir Dich in ein aktives Projektteam. So sammelst Du Erfahrungen im Rahmen größerer Entwicklungsprojekte mit mehreren Beteiligten. Hier lernst Du z. B. wie gemeinsam an einem Quellcode gearbeitet wird, was Level 2 und Level 3 Tests sind, wie sich Verbesserungsvorschläge konstruktiv einbringen lassen und vieles mehr.',
-      imageUrl: '../../assets/images/explanation-images/kooperation.svg',
-      imageAlt: 'Illustration eines Mannes und einer Frau die Puzzleteile halten.',
-    },
-  ];
+  @Input() listElements: TextImageInterface[];
 
   constructor() {}
 }
--- src/app/sections/landing-pages/intern-page/intern-page.component.ts
+++ src/app/sections/landing-pages/intern-page/intern-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MetaService } from 'src/app/modules/core/services/meta.service';
 import { Review } from '../../../modules/core/interfaces/review.interface';
-
+import { TextImageInterface } from 'src/app/modules/core/interfaces/text-image.interface';
 // import Swiper core and required modules
 import SwiperCore, { Autoplay, Navigation, Pagination } from 'swiper';
 
@@ -28,6 +28,27 @@ export class InternPageComponent implements OnInit {
     reviewerTitle: 'Florian Steinhanses',
     reviewerSubtitle: 'Praktikant und Abschlussarbeitskandidat',
   };
+
+  listElements: TextImageInterface[] = [
+    {
+      title: 'Akademie',
+      text: 'Durch Tutorials in Form von Texten, Videokursen und Spielen bekommst Du einen ersten Einstieg in die Grundlagen der App- und Webentwicklung. \n\nUnser Anliegen ist es, mit der Akademie allen Interessierten einen idealen Einstiegspunkt in den Bereich der Softwareentwicklung bieten zu können. Ob mit oder ohne Vorkenntnisse. \n\nUm dieses Ziel zu erreichen, entwickeln wir die Akademie ständig weiter und freuen uns auf Dein Feedback.',
+      imageUrl: '../../assets/images/explanation-images/academy.svg',
+      imageAlt: 'Illustration eines Mannes der auf Büchern steht.',
+    },
+    {
+      title: 'Eigenes Projekt',
+      text: 'Nachdem Du Dir die Grundlagen angeeignet hast, erstellst Du basierend auf Deinem neuen Wissen Dein erstes eigenes und vollständiges Projekt. Dabei wirst Du sowohl einen Client als auch einen Server bauen und diese miteinander verbinden. Wichtige Bedingung: Es wird kein „Schubladenprojekt“. Daher such Dir ein Projekt aus, das für eine reale Zielgruppe einen echten Mehrwert bietet.',
+      imageUrl: '../../assets/images/explanation-images/projekt.svg',
+      imageAlt: 'Illustration eines Mannes der malt.',
+    },
+    {
+      title: 'Teamarbeit',
+      text: 'Sofern die Dauer des Praktikums es zulässt und Du die ersten beiden Schritte erfolgreich gegangen bist, integrieren wir Dich in ein aktives Projektteam. So sammelst Du Erfahrungen im Rahmen größerer Entwicklungsprojekte mit mehreren Beteiligten. Hier lernst Du z. B. wie gemeinsam an einem Quellcode gearbeitet wird, was Level 2 und Level 3 Tests sind, wie sich Verbesserungsvorschläge konstruktiv einbringen lassen und vieles mehr.',
+      imageUrl: '../../assets/images/explanation-images/kooperation.svg',
+      imageAlt: 'Illustration eines Mannes und einer Frau die Puzzleteile halten.',
+    },
+  ];
 
   constructor(private metaService: MetaService) {}
 
