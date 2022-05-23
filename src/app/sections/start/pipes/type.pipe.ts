3fb4c031f1ecb22c4c1bc48da860ce3df71e3661
Daniel König <daniel.koenig@lenne.tech>
2022-05-23 12:12:58 +0200

    refactor(LTAK-70): refactored subscription page

--- src/app/sections/start/pipes/type.pipe.ts
+++ src/app/sections/start/pipes/type.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TypePipe implements PipeTransform {
   transform(value: any, args?: any): any {
-    return value.filter((member) => {
-      return member.type.includes(args);
+    return value.filter((item) => {
+      return item.type.includes(args);
     });
   }
 }
